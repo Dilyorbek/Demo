868ba0646be632a4cdcbd61e6e37664cf0380f5c
Dilyorbek <alimov.dilyorbek@gmail.com>
2022-10-18 20:03:32 +0200

    Tags added

---
+++ app/src/main/java/uz/msit/demo/feature/users/presentation/preview_param_provider/Tags.kt
@@ -0,0 +1,22 @@
+package uz.msit.demo.feature.users.presentation.preview_param_provider
+
+object Tags {
+
+    const val TOP_BAR = "TOP_BAR"
+    const val BACK_BUTTON = "Back"
+
+    const val ERROR_VIEW = "error"
+    const val ERROR_MESSAGE = "errorMessage"
+    const val ERROR_BUTTON = "errorButton"
+
+    const val LOADING_VIEW = "loading"
+    const val LOADING_INDICATOR_VIEW = "loadingIndicator"
+
+    const val USER_ITEM_VIEW = "userItem"
+    const val USER_LIST_VIEW = "userList"
+    const val USER_IMAGE= "userImage"
+
+    const val USER_DETAILS_ITEM_VIEW= "userDetailsItem"
+    const val USER_DETAILS_HEADER_VIEW= "userDetailsHeader"
+    const val USER_DETAILS_LIST_VIEW= "userDetailsList"
+}
